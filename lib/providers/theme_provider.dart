8a1be4899e67725d274d9b4b7f809d0b9f548f4f
Yethum Danith <yethumdanith2002@gmail.com>
2026-04-26 19:19:13 +0530

    Bug FIxed Dark mode

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider with ChangeNotifier {
   bool _isDarkMode = false;
+  
+  final String _themePreferenceKey = "is_dark_mode"; 
 
   bool get isDarkMode => _isDarkMode;
-
   ThemeData get currentTheme => _isDarkMode ? _darkTheme : _lightTheme;
 
-  void toggleTheme() {
-    _isDarkMode = !_isDarkMode;
-    notifyListeners();
+  ThemeProvider() {
+    _loadThemeFromPrefs();
   }
 
+  Future<void> _loadThemeFromPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isDarkMode = prefs.getBool(_themePreferenceKey) ?? false;
+    notifyListeners(); 
+  }
+
+//check the previous mode dark or light 
+  void toggleTheme() async {
+    _isDarkMode = !_isDarkMode;
+    notifyListeners(); 
+    
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_themePreferenceKey, _isDarkMode); 
+  }
+
+  
   final ThemeData _lightTheme = ThemeData(
     brightness: Brightness.light,
     primarySwatch: Colors.green,
@@ -25,7 +42,7 @@ class ThemeProvider with ChangeNotifier {
   final ThemeData _darkTheme = ThemeData(
     brightness: Brightness.dark,
     primarySwatch: Colors.green,
-    scaffoldBackgroundColor: const Color(0xFF121212), // Standard dark background
+    scaffoldBackgroundColor: const Color(0xFF121212), 
     appBarTheme: const AppBarTheme(
       backgroundColor: Color(0xFF1E1E1E),
       foregroundColor: Colors.white,
